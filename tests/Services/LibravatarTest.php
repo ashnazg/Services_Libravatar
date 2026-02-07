9abae61f83544225fb4006ec7cb65bc99006b063
Chuck Burgess <ashnazg@php.net>
2026-02-07 11:21:20 -0600

    switch to mockbuilder

--- tests/Services/LibravatarTest.php
+++ tests/Services/LibravatarTest.php
@@ -451,10 +451,11 @@ class Services_LibravatarTest extends PHPUnit_Framework_TestCase
 
     protected function loadSLMock()
     {
-        $this->sl = $this->getMock('Services_Libravatar', array('srvGet'));
-        $this->sl->expects($this->once())
-            ->method('srvGet')
-            ->will($this->returnValue('example.org'));
+        //$this->sl = $this->getMock('Services_Libravatar', array('srvGet'));
+        //$this->sl->expects($this->once())->method('srvGet')->will($this->returnValue('example.org'));
+
+        $this->sl = $this->getMockBuilder(Services_Libravatar::class)->onlyMethods(array('srvGet'))->getMock();
+        $this->sl->expects($this->once())->method('srvGet')->will($this->returnValue('example.org'));
     }
 }
 
